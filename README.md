4f02e72c26c7807143e8a0b503dd6db0bc742f1b
unknown <vazquezt101@gmail.com>
2019-06-23 20:11:24 -0700

    adding dynamo table

---
+++ README.md
@@ -0,0 +1,18 @@
+# Traffic Data Collector
+
+This project creates a aws lambda function that reads every 5 minutes the traffic data from the google maps distance matrix api, the the responses are saved int a s3 bocket.
+
+The porpuse of this is to create the data source used in a ETL process.
+
+## Setup
+
+```shell
+# install pipenv if not already installed, python 3.7 required
+pip install pipenv
+# Install python dependencies
+pipenv install
+# Install node dependencies, nodejs 10+ with npm 6+ required
+npm install
+#deploy, you need to have your aws credentials configured 
+npm run deploy -- --GOOGLE_MAPS_API_KEY <put_here_your_api_key>
+```
--- traffict_data_collector.py
+++ traffict_data_collector.py
@@ -9,8 +9,16 @@ from urllib.parse import quote_plus
 BASE_URL = 'https://maps.googleapis.com/maps/api/distancematrix'
 GOOGLE_MAPS_API_KEY = os.environ['GOOGLE_MAPS_API_KEY']
 TRAFFIC_DATA_SOURCE_BUCKET_NAME = os.environ['TRAFFIC_DATA_SOURCE_BUCKET_NAME']
+ORIGIN_DESTINATION_TABLE_NAME = os.environ['ORIGIN_DESTINATION_TABLE_NAME']
+
 
 def get_trafct_data(origin, destination, key):
+    """
+    The function that makes the request to google api
+        origin: a string in url format indicating the starting point 
+        destination: a string in url format indicating the end point
+        key: the google api key
+    """
     request_url = "{url}/json?origins={origin}&destinations={destination}&departure_time=now&key={key}".format(
         url=BASE_URL,
         origin=origin, 
@@ -18,22 +26,32 @@ def get_trafct_data(origin, destination, key):
         key=key)
     return requests.get(request_url)
 
-origin = "Priv. Cereme, Urbi Villas del Prado 2, Tijuana"
-destination = "SRT, Tijuana"
-
 def handler(event, context):
-    client = boto3.client('s3')
-    response = get_trafct_data(quote_plus(origin), quote_plus(destination), GOOGLE_MAPS_API_KEY)
-    json_obj = json.loads(response.content)
-    yaml_str = yaml.dump(json_obj)
-    today = datetime.datetime.now()
-    key = '{year}/{month}/{day}/{hh_mm_ss}_traffic_data.yaml'.format(
-        year=today.year,
-        month=today.month,
-        day=today.day,
-        hh_mm_ss=today.strftime('%H_%M_%S')
-    )
-    client.put_object(Body=yaml_str, Bucket=TRAFFIC_DATA_SOURCE_BUCKET_NAME, Key=key)
+    """
+    This function is the entry point of the lambda.
+    It reads the items from a table and based in the origin and destination 
+    gets the traffic data and saves it in a s3 bucket.
+    """
+    s3 = boto3.client('s3')
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(ORIGIN_DESTINATION_TABLE_NAME)
+
+    scan = table.scan()
+    for item in scan['Items']:
+        id = item['uuid']
+        origin = item['origin']
+        destination = item['destination']
+        response = get_trafct_data(quote_plus(origin), quote_plus(destination), GOOGLE_MAPS_API_KEY)
+        json_obj = json.loads(response.content)
+        yaml_str = yaml.dump(json_obj)
+        today = datetime.datetime.now()
+        key = '{id}/{date}_traffic_data.yaml'.format(
+            id=id,
+            date=today.strftime("%Y_%m_%d_%H_%M")
+        )
+        s3.put_object(Body=yaml_str, Bucket=TRAFFIC_DATA_SOURCE_BUCKET_NAME, Key=key)
+
+
     body = {
         "message": "executed successfully!",
         "input": event
