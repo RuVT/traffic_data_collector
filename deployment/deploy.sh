c952675380e32b2a386bb7235692941d87b4c198
Palacio <cpalacio@cetys.edu.mx>
2019-06-24 23:13:43 -0700

    Installing pipenv in the directory.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,7 +6,8 @@ then
     echo "Usage: deploy.sh <GOOGLE_API_KEY> <BUILD_NUMBER>"
     exit 1
 fi
-pip3 install pipenv --user
+
 pushd $BUILD_NUMBER
+pipenv install
 serverless deploy --GOOGLE_MAPS_API_KEY  --alias $BUILD_NUMBER
 popd
